9a0aa356796fe0101446a9cb19fbb9b280ec3c93
UssraIn <164646785+UssraIn@users.noreply.github.com>
2024-08-10 11:50:18 +0200

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Project_Fichier_DXF
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.buttonExport = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(698, 446);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 46);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // buttonExport
+            // 
+            this.buttonExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExport.Location = new System.Drawing.Point(425, 446);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(158, 46);
+            this.buttonExport.TabIndex = 6;
+            this.buttonExport.Text = "Export Cercle";
+            this.buttonExport.UseVisualStyleBackColor = true;
+            this.buttonExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(180, 446);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(137, 46);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(180, 121);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(655, 51);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(175, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(651, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Exporte DXF...............................................................dxf  ";
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pic.Location = new System.Drawing.Point(180, 214);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(655, 194);
+            this.pic.TabIndex = 10;
+            this.pic.TabStop = false;
+            this.pic.Paint += new System.Windows.Forms.PaintEventHandler(this.pic_Paint_1);
+            this.pic.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_MouseDown_1);
+            this.pic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_MouseMove_1);
+            this.pic.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pic_MouseUp_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(726, 376);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "By:YOUSRA";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(37, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 46);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 534);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonExport);
+            this.Controls.Add(this.button5);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonExport;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Project_Fichier_DXF.Form3;
+/**
+ *  else if (entity is Circle)
+                   {
+                       Circle circle = (Circle)entity;
+                       // Add code to export Circle entity
+                   }
+                   else if (entity is Arc)
+                   {
+                       Arc arc = (Arc)entity;
+                       // Add code to export Arc entity
+                   }
+                    **/
+
+namespace Project_Fichier_DXF
+{
+   public partial class Form2 : Form
+   {
+       private List<Circle> circles = new List<Circle>(); // List to hold circles
+        private List<Arc> arcs = new List<Arc>(); // List to hold circles
+
+        // un booleen qui indique si l'utilisateur est en train de dessiner une linge
+        private bool drawing = false;
+       // Represente le point de depart
+       private Point startPoint;
+       // Represente le nom du fichier DXF a exporter
+       public string fileName = "C:\\Test.dxf";
+       public Form2()
+       {
+           InitializeComponent();
+           textBox2.Text = fileName;
+           pic.MouseDown += new MouseEventHandler(pic_MouseDown_1);
+           pic.MouseMove += new MouseEventHandler(pic_MouseMove_1);
+           pic.MouseUp += new MouseEventHandler(pic_MouseUp_1);
+           pic.Paint += new PaintEventHandler(pic_Paint_1);
+       }
+
+       private void pictureBox1_Click(object sender, EventArgs e)
+       {
+
+       }
+       private void ExportToDXF(string fileName)
+       {
+           using (StreamWriter writer = new StreamWriter(fileName))
+           {
+               // Write DXF header
+               writer.WriteLine("0");
+               writer.WriteLine("SECTION");
+               writer.WriteLine("2");
+               writer.WriteLine("HEADER");
+               writer.WriteLine("0");
+               writer.WriteLine("ENDSEC");
+               writer.WriteLine("0");
+               writer.WriteLine("SECTION");
+               writer.WriteLine("2");
+               writer.WriteLine("TABLES");
+               writer.WriteLine("0");
+               writer.WriteLine("TABLE");
+               writer.WriteLine("2");
+               writer.WriteLine("LAYER");
+               writer.WriteLine("70");
+               writer.WriteLine("1");
+               writer.WriteLine("0");
+               writer.WriteLine("LAYER");
+               writer.WriteLine("2");
+               writer.WriteLine("Layer1");
+               writer.WriteLine("70");
+               writer.WriteLine("0");
+               writer.WriteLine("62");
+               writer.WriteLine("7");
+               writer.WriteLine("6");
+               writer.WriteLine("CONTINUOUS");
+               writer.WriteLine("0");
+               writer.WriteLine("ENDTAB");
+               writer.WriteLine("0");
+               writer.WriteLine("ENDSEC");
+               writer.WriteLine("0");
+               writer.WriteLine("SECTION");
+               writer.WriteLine("2");
+               writer.WriteLine("ENTITIES");
+
+               // Write circles
+               foreach (var circle in circles)
+               { 
+                       writer.WriteLine("0");
+                       writer.WriteLine("CIRCLE");
+                       writer.WriteLine("8");
+                       writer.WriteLine(circle.Layer);
+                       writer.WriteLine("10");
+                       writer.WriteLine(circle.Center.X);
+                       writer.WriteLine("20");
+                       writer.WriteLine(circle.Center.Y);
+                       writer.WriteLine("30");
+                       writer.WriteLine(circle.ZCenter);
+                       writer.WriteLine("40");
+                       writer.WriteLine(circle.Radius); 
+               }
+               // Write Arc
+                foreach (var arc in arcs)
+                {
+                    writer.WriteLine("0");
+                    writer.WriteLine("ARC");
+                    writer.WriteLine("8");
+                    writer.WriteLine(arc.Layer);
+                    writer.WriteLine("10");
+                    writer.WriteLine(arc.Center.X);
+                    writer.WriteLine("20");
+                    writer.WriteLine(arc.Center.Y);
+                    writer.WriteLine("30");
+                    writer.WriteLine(arc.ZCenter);
+                    writer.WriteLine("40");
+                    writer.WriteLine(arc.Radius);
+                    writer.WriteLine("50");
+                    writer.WriteLine(arc.StartAngle);
+                    writer.WriteLine("51");
+                    writer.WriteLine(arc.EndAngle);
+                }
+
+                // End entities section
+                writer.WriteLine("0");
+               writer.WriteLine("ENDSEC"); //La fin de section
+               writer.WriteLine("0");
+               writer.WriteLine("EOF"); //La fin du fichier DXF
+           }
+       }
+
+
+       // Circle class
+       public class Circle
+       {
+           public PointF Center { get; set; }
+           public float ZCenter { get; set; }
+           public float Radius { get; set; }
+           public string Layer { get; set; }
+           public int Color { get; set; }
+           public string LineType { get; set; }
+           public float LineTypeScale { get; set; }
+
+           public Circle(PointF center, float radius, string layer = "Layer1", int color = 0, string lineType = "", float lineTypeScale = 1.0f)
+           {
+               Center = center;
+               Radius = radius;
+               ZCenter = 0; // Default Z coordinate
+               Layer = layer;
+               Color = color;
+               LineType = lineType;
+               LineTypeScale = lineTypeScale;
+           }
+       }
+
+       private void pic_MouseDown_1(object sender, MouseEventArgs e)
+       {
+           if (e.Button == MouseButtons.Left)
+           {
+               drawing = true;
+               startPoint = e.Location; // Capture the starting point of the circle
+           }
+       }
+
+       private void pic_MouseMove_1(object sender, MouseEventArgs e)
+       {
+           if (drawing)
+           {
+               pic.Invalidate(); // Refresh the PictureBox to redraw
+           }
+
+       }
+
+       private void pic_Paint_1(object sender, PaintEventArgs e)
+       {
+            // Draw existing circles
+            foreach (var circle in circles)
+            {
+                e.Graphics.DrawEllipse(Pens.Black, circle.Center.X - circle.Radius, circle.Center.Y - circle.Radius, circle.Radius * 2, circle.Radius * 2);
+            }
+
+            // Draw existing arcs
+            foreach (var arc in arcs)
+            {
+                using (Pen pen = new Pen(Color.Black))
+                {
+                    e.Graphics.DrawArc(pen, arc.Center.X - arc.Radius, arc.Center.Y - arc.Radius, arc.Radius * 2, arc.Radius * 2, arc.StartAngle, arc.EndAngle - arc.StartAngle);
+                }
+            }
+            // Draw the currently being created shape
+            if (drawing)
+            {
+                float radius = (float)Math.Sqrt(Math.Pow(pic.PointToClient(MousePosition).X - startPoint.X, 2) +
+                                                 Math.Pow(pic.PointToClient(MousePosition).Y - startPoint.Y, 2));
+                e.Graphics.DrawEllipse(Pens.Red, startPoint.X - radius, startPoint.Y - radius, radius * 2, radius * 2);
+            }
+        }
+        // Arc class
+    public class Arc
+        {
+            public PointF Center { get; set; }
+            public float ZCenter { get; set; }
+            public float Radius { get; set; }
+            public float StartAngle { get; set; }
+            public float EndAngle { get; set; }
+            public string Layer { get; set; }
+
+            public Arc(PointF center, float radius, float startAngle, float endAngle, string layer = "Layer1")
+            {
+                Center = center;
+                Radius = radius;
+                StartAngle = startAngle;
+                EndAngle = endAngle;
+                ZCenter = 0; // Default Z coordinate
+                Layer = layer;
+            }
+        }
+
+        private void pic_MouseUp_1(object sender, MouseEventArgs e)
+       {
+            if (drawing)
+            {
+                drawing = false;
+                float radius = (float)Math.Sqrt(Math.Pow(e.X - startPoint.X, 2) + Math.Pow(e.Y - startPoint.Y, 2));
+
+                // Ask the user to choose the type of entity
+                var result = MessageBox.Show("Do you want to add this as a Circle? (Click 'No' to add as an Arc)", "Choose Entity Type", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    // Add the new circle to the list
+                    circles.Add(new Circle(startPoint, radius));
+                }
+                else
+                {
+                    // For simplicity, we’ll assume an arc from 0 to 180 degrees
+                    arcs.Add(new Arc(startPoint, radius, 0, 180));
+                }
+
+                pic.Invalidate(); // Refresh to show the new shape
+            }
+
+        }
+
+       private void button5_Click(object sender, EventArgs e)
+       {
+           // Clear Pour supprimer tout les lignes
+           circles.Clear();
+           // Invalidate pour faire mise a jour de pictureBox
+           pic.Invalidate();
+       }
+
+       private void button4_Click(object sender, EventArgs e)
+       {
+           DialogResult result = MessageBox.Show("Are you sure you want to exit  ?", "Exit confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+           if (result == DialogResult.Yes)
+           {
+               Application.Exit();
+           }
+       }
+
+       private void buttonExport_Click(object sender, EventArgs e)
+       {
+           if (circles.Count == 0)
+           {
+               MessageBox.Show("No circles to export.");
+               return;
+           }
+
+           SaveFileDialog saveFileDialog = new SaveFileDialog
+           {
+               //Type de fichier
+               Filter = "DXF files (*.dxf)|*.dxf",
+               //Titre de fenter
+               Title = "Save DXF File",
+               FileName = textBox2.Text
+           };
+
+           if (saveFileDialog.ShowDialog() == DialogResult.OK)
+           {
+               fileName = saveFileDialog.FileName;
+               ExportToDXF(fileName);
+               MessageBox.Show("Export completed.");
+           }
+       }
+
+       private void Form2_Load(object sender, EventArgs e)
+       {
+
+       }
+
+       private void button1_Click(object sender, EventArgs e)
+       {
+            drawing = true;
+            MessageBox.Show("Click on the PictureBox to start drawing circles.");
+       }
+   }
+}
+
